11a7c036c30174c710a7b5558944ebcd122f60f8
灯 <@akari>
2014-11-24 23:54:17 +0900

    * Hello -> Index

---
+++ src/main/java/jp/co/javapractise/todowatch/Controller/IndexController.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.javapractise.todowatch.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+/**
+ *
+ * @author akari
+ */
+@Controller
+@RequestMapping(value = "/index")
+public class IndexController {
+    protected static final String VIEW = "helloworld";
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public String handleGet(Model model) {
+        return VIEW;
+    }
+}
